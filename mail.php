19b36e91b5eac7f8452ba254b0bb0f7d6e5fc78b
Andrey Barmaley <pengo@tsn.com.ua>
2020-01-04 19:02:40 +0200

    First commit

---
+++ mail.php
@@ -0,0 +1,11 @@
+
+<?php
+$to      = 'pengo@tsn.com.ua';
+$subject = 'the subject';
+$message = 'hello';
+$headers = 'From: webmaster@example.com' . "\r\n" .
+    'Reply-To: webmaster@example.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+?>
